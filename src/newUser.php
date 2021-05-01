a72a7b5712fc8ac370fdacf5e564a1c182959652
LeonardoHysesani <lhysesani@gmail.com>
2021-05-02 00:23:29 +0300

    Fixed signup proccess and minified background image for faster loading

--- src/newUser.php
+++ src/newUser.php
@@ -11,23 +11,60 @@
 	}
 
 	//CHECKING VALIDITY OF DATA
-	$result = $conn -> query("SELECT UID FROM UsersTable WHERE USERNAME = '$user'");
+	$result = $conn -> query("SELECT UUID FROM UsersTable WHERE USERNAME = '$user'");
 	$row = $result -> fetch_row();
 	switch ($row) {
 		case !NULL:
-			echo("<center><h1 style='color:white'>USERNAME IS TAKEN</h1><center>");
+			echo('<center><h1 style="color:white">USERNAME IS TAKEN</h1><center>
+			<center>
+							<br>
+							<br>
+							<br>
+							<br>
+							<br>
+							<br>
+							<br>
+							<a href="Login.html" style="text-decoration-line: none; padding: 10px; border: 1px solid green; background-color: green; color: white">
+								Login
+							</a>
+						</center>
+			');
 			break;
 
 		default:
 			if ($passw == $confpassw) {
-					if ($conn -> query("CREATE TABLE sampledb.$user ( TYPE TEXT NULL DEFAULT NULL , USERNAME TEXT NULL DEFAULT NULL , PASSWORD TEXT NULL DEFAULT NULL , ID INT NOT NULL AUTO_INCREMENT, PRIMARY KEY (ID)) ENGINE = InnoDB") == TRUE) {
-						$conn -> query("INSERT INTO UsersTable (USERNAME, PASSWORD) VALUES ('$user', '$passw')");
-						printf("<h1 style='color:white'>Account created. You can now login.</h1>");
+					if ($conn -> query("CREATE TABLE vault.$user ( TYPE TEXT NULL DEFAULT NULL , USERNAME TEXT NULL DEFAULT NULL , PASSWORD TEXT NULL DEFAULT NULL , ID INT NOT NULL AUTO_INCREMENT, PRIMARY KEY (ID)) ENGINE = InnoDB") == TRUE) {
+						$conn -> query("INSERT INTO UsersTable (USERNAME, PASSWORD_HASH) VALUES ('$user', '$passw')");
+						printf('<h1 style="color:white">Account created. You can now login.</h1>
+						<center>
+							<br>
+							<br>
+							<br>
+							<br>
+							<br>
+							<br>
+							<br>
+							<a href="Login.html" style="text-decoration-line: none; padding: 10px; border: 1px solid green; background-color: green; color: white">
+								Login
+							</a>
+						</center>');
 					}
-					break;
 			}
 			else{
-				echo "<center><h1 style='color:white'>PASSWORDS DON'T MATCH</h1><center>";
+				echo "<center><h1 style='color:white'>PASSWORDS DON'T MATCH</h1>
+				<center>
+							<br>
+							<br>
+							<br>
+							<br>
+							<br>
+							<br>
+							<br>
+							<a href='Signup.html' style='text-decoration-line: none; padding: 10px; border: 1px solid green; background-color: green; color: white'>
+								Back
+							</a>
+						</center>
+				<center>";
 			}
 	}
 ?>
@@ -37,15 +74,6 @@
  	<title>Account creation</title>
  </head>
  <body style="background-color: #ffccff; background-image: url(/img/tech4.jpg);background-repeat: no-repeat;background-size: 100%; background-blend-mode: multiply; background-repeat: repeat-y;">
- 	<center>
- 		<br>
- 		<br>
- 		<br>
- 		<br>
- 		<br>
- 		<br>
- 		<br>
- 		<a href="Login.html" style="text-decoration-line: none; padding: 10px; border: 1px solid green; background-color: green; color: white">Login</a>
- 	</center>
+ 	
  </body>
  </html>
